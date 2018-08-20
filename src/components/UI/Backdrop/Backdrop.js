e1c72ada7d201e02a56710c347b7812bf15a3f48
Joseph Arias <arias.joseph.a@gmail.com>
2018-08-19 23:10:18 -0400

    Creating Axios instance

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,9 @@ import React from "react";
 import classes from "./Backdrop.css";
 
 const backDrop = props => {
-  return props.show ? <div className={classes.Backdrop} onClick={props.clicked} /> : null;
+  return props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked} />
+  ) : null;
 };
 
 export default backDrop;
